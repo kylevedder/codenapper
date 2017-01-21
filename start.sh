092ddebafc2b2f34deec37a97f3cb29d35ca0f16
kylevedder <kyle.c.vedder@gmail.com>
2017-01-21 01:32:20 -0500

    Fixed comparison

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 date >> "$DIR/server.log"
-if [ -z ${SOURCEPATH+x} ];
+if [ -z "$SOURCEPATH" ];
 then
     echo "SOURCEPATH is unset, please set it.";
 else
